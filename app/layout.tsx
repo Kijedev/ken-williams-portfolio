6b91f9dee9d94f1549759c298adc1e80b6103201
Kijeosowo <pelumiwilliams23@gmail.com>
2026-04-17 12:41:09 +0100

    fix: routing issue during deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ClientShell from "./components/Clientshell/page";
+import SmoothScroll from "./components/SmoothScroll";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer/page"
+// import ClientShell from "./components/Clientshell/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -24,9 +22,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <ClientShell>{children}</ClientShell>
+        <SmoothScroll>
+          <Navbar />
+          {children}
+          <Footer />
+        </SmoothScroll>
       </body>
     </html>
   );
