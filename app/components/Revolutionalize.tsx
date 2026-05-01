e4e8714d9f3cb74ff73cddfe47f2a8a760575db9
Kijeosowo <pelumiwilliams23@gmail.com>
2026-05-01 01:28:25 +0100

    removed the glow in the revolutionalize section

--- app/components/Revolutionalize.tsx
+++ app/components/Revolutionalize.tsx
@@ -18,11 +18,11 @@ export default function Revolutionalize() {
 
     return (
         <section ref={ref} className="bg-black flex flex-col lg:h-screen h-[50vh] relative z-50 items-center justify-center overflow-hidden">
-            <div
+            {/* <div
                 className="pointer-events-none absolute top-0 left-1/2 -translate-x-1/2 w-[900px] h-[600px] opacity-[0.05] z-1"
                 style={{ background: "radial-gradient(ellipse, #fff 0%, transparent 65%)" }}
                 aria-hidden="true"
-            />
+            /> */}
             <div className="flex flex-col lg:gap-0 gap-5 lg:pt-10 pt-5 px-4 lg:px-0 py-12 lg:py-0">
                 {/* WHATS */}
                 <motion.h1
